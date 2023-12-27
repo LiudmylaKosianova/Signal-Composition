c896677a36c2f7db3bbd0bc5eb1ed029b0539a66
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-27 12:38:54 +0100

    start

---
+++ square_wave.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+//time series range (0s to 1ms, 1 cycle of 1kHz wave)
+#define Tmin 0.0
+#define Tmax 1e-3
+#define Npts 100
+
+//frequency components of square wave
+#define f1 1000.0
+#define f3 3000.0
+#define f5 5000.0
+#define f7 7000.0
+#define f9 9000.0
+
+//magnitude components of square wave
+#define v1 0.637
+#define v3 0.212
+#define v5 0.127
+#define v7 0.091
+#define v9 0.071
+
+#define PI 3.141
+
+int main(void){
+
+    return 0;
+}
